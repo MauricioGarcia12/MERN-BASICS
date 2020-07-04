79cd7abadec38761e6b9ad3655833ef016ae412e
unknown <mainsaw_12@live.com.mx>
2020-07-03 20:49:35 -0500

    Creating Routes and Components

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Switch, Route} from 'react-router-dom'
+import Navigation from './components/Navigation';
+import CreateNote from './components/CreateNote';
+import CreateUser from './components/CreateUser';
+import NoteList from './components/NoteList';
+
+function App() {
+  return (
+  
+    <Switch>
+      <Navigation/>
+      <Route  path="/" exact  component={NoteList} />
+      <Route path="/edit/:id" component={CreateNote} />
+      <Route path="/create" component={CreateNote} />
+      <Route path="/user" component={CreateUser} />
+
+    </Switch>
+  );
+}
+
+export default App;
